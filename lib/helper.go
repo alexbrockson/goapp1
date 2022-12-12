ca6181ea716af43d0824052b7e7a6e258cf999d6
Alex Brockson <alexbrockson@gmail.com>
2022-12-12 18:33:57 -0500

    youtube link helper comment in helper.go

--- lib/helper.go
+++ lib/helper.go
@@ -7,7 +7,7 @@ import (
 func Sum(values ...int) int {
 	fmt.Println(values)
 	result := 0
-	for _, v := range values {
+	for _, v := range values { //https://youtu.be/YS4e4q9oBaU?t=13076
 		result += v
 	}
 	return result
